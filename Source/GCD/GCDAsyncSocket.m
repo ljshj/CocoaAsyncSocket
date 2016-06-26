d74d89d7bd2d4f604d950c5066da93a992dd0ea6
ljshj <jinshanl@gmail.com>
2016-06-26 23:13:51 +0800

    Update GCDAsyncSocket.m

--- Source/GCD/GCDAsyncSocket.m
+++ Source/GCD/GCDAsyncSocket.m
@@ -8059,6 +8059,8 @@ static void CFWriteStreamCallback (CFWriteStreamRef stream, CFStreamEventType ty
 					// Found IPv4 address.
 					// Wrap the native address structure, and add to results.
 					
+					if (((struct sockaddr_in *)res->ai_addr)->sin_port == 0)
+                        			((struct sockaddr_in *)res->ai_addr)->sin_port = htons(port);
 					NSData *address4 = [NSData dataWithBytes:res->ai_addr length:res->ai_addrlen];
 					[addresses addObject:address4];
 				}
@@ -8067,6 +8069,8 @@ static void CFWriteStreamCallback (CFWriteStreamRef stream, CFStreamEventType ty
 					// Found IPv6 address.
 					// Wrap the native address structure, and add to results.
 					
+					if (((struct sockaddr_in6 *)res->ai_addr)->sin6_port == 0)
+                        			((struct sockaddr_in6 *)res->ai_addr)->sin6_port = htons(port);
 					NSData *address6 = [NSData dataWithBytes:res->ai_addr length:res->ai_addrlen];
 					[addresses addObject:address6];
 				}
